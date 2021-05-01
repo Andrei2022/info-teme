0d580ebbb7b504812fb4006bc2671d3b1c788e92
Andrei2022 <andrei.dorin09@yahoo.com>
2021-05-01 17:30:36 +0300

    Add tema 28.04.2021

---
+++ tema ex 2 28.04.2021.cpp
@@ -0,0 +1,39 @@
+﻿//Se consideră subprogramul multiplu, cu doi parametri, care:
+//– primeşte prin intermediul parametrilor a şi k două numere întregi de cel mult 4 cifre;
+//– returnează cel mai mic multiplu al lui k mai mare sau egal cu a.
+
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main(int a, int k)
+{
+    int max;
+    for (a = 0; a <= 9999; a++)
+    
+        for (k = 0; k <= 9999; k++) 
+        {
+            if (k > 9999 || a > 9999)
+            {
+                printf("numar prea mare");
+                break;
+            }
+        }
+    
+    printf("Scrie 2 numere pozitive ");
+    scanf("%d %d", &a, &k);
+
+    // maximum number between n1 and n2 is stored in min
+    max = (a > k) ? a : k;
+
+    while (1) {
+        if (max % a == 0 && max % k == 0) {
+            printf("Cel mai mic multipplu comun intre %d si %d este %d.", a, k, max);
+            break;
+        }
+        ++max;
+    }
+    return 0;
+}
+
+
+
---
+++ tema ex 3 28.04.2021.cpp
@@ -0,0 +1,27 @@
+﻿//3.Subprogramul sfx primeşte prin singurul său parametru, x, un număr natural din intervalul [100,2000000000] 
+//şi returnează valoarea 1 dacă ultimele trei cifre ale numărului sunt în ordine strict descrescătoare 
+//sau valoarea 0 în caz contrar.
+//Exemplu: dacă x = 24973 se va returna valoarea 1.
+
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int sfx(int x)
+{
+	int a, b, c, k;
+	k = 0;
+	a = x % 1000 / 100; b = x % 100 / 10;
+	c = x % 10;
+	if (a > b && b > c) k = 1;
+	return k;
+}
+int main()
+{
+	int n;
+	scanf("%d", &n); 
+	
+	if (sfx(n) == 1)
+		printf("da"); 
+	else 
+		printf("nu");
+}
